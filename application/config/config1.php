4ad56142bc923afa969062c25da76f12b319d270
root <root@project.report>
2018-05-03 15:52:19 +0700

    versi 1.3 Perubahan required field pada form update project yang belum di assignD qt untuk role qt kordinator

--- application/config/config1.php
+++ application/config/config1.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | your installation, but due to security concerns the hostname will be set
 | to $_SERVER['SERVER_ADDR'] if available, or localhost otherwise.
 | The auto-detection mechanism exists only for convenience during
-| development and MUST NOT be used in production!
+| development and MUST NOT be used in production!1
 |
 | If you need to allow multiple domains, remember that this file is still
 | a PHP script and you can easily do that on your own.
